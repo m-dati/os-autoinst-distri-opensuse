a0cfa32b9f62658277192a65d6168ab4b2ce0d0d
m-dati <mdati@suse.com>
2024-07-30 16:53:56 +0200

    Timeout increased for vmware in bootloader assert

--- lib/bootloader_setup.pm
+++ lib/bootloader_setup.pm
@@ -468,17 +468,18 @@ sub uefi_bootmenu_params {
         die "Failed to edit grub2 after $max_tries tries.";
     }
 
+    my $timeout = (get_var('FLAVOR') =~ /vmware/i) ? 60 : 10;
     # Kiwi in TW uses grub2-mkconfig instead of the custom kiwi config
     # Locate gfxpayload parameter and update it
     if (is_jeos && (!is_sle('=12-SP5') || is_opensuse)) {
-
+        
         for (1 .. 3) { send_key "down"; }
         send_key "end";
         # delete "keep" word
         for (1 .. 4) { send_key "backspace"; }
         # hardcoded the value of gfxpayload to 1024x768
         type_string "1024x768";
-        assert_screen "gfxpayload_changed", 10;
+        assert_screen "gfxpayload_changed", $timeout;
         # back to the entry position
         send_key "home";
         for (1 .. 6) { send_key "down"; }
@@ -500,7 +501,7 @@ sub uefi_bootmenu_params {
         for (1 .. 4) { send_key "backspace"; }
         # hardcoded the value of gfxpayload to 1024x768
         type_string "1024x768";
-        assert_screen "gfxpayload_changed", 10;
+        assert_screen "gfxpayload_changed", $timeout;
         # back to the entry position
         send_key "home";
         for (1 .. 2) { send_key "up"; }
